898da68a6bc93637429a6d1d1acacd36705d837e
Rivanildo Júnior <rivanildo_junior7@hotmail.com>
2022-06-13 21:57:04 -0300

    config[]: start project dashboard and configuration routes

---
+++ dashboard/src/router/index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const Home = () => import('../views/Home/index.vue')
+const Feedbacks = () => import('../views/Feedbacks/index.vue')
+const Credentials = () => import('../views/Credentials/index.vue')
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/feedbacks',
+    name: 'Feedbacks',
+    component: Feedbacks,
+    meta: {
+      hasAuth: true
+    }
+  },
+  {
+    path: '/Credentials',
+    name: 'Credentials',
+    component: Credentials,
+    meta: {
+      hasAuth: true
+    }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: { name: 'Home' }
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory('/'),
+  routes
+})
+
+export default router
