7dcc633eec861d114a42de6522dd8da6df52aa4a
Rivanildo Júnior <rivanildo_junior7@hotmail.com>
2022-06-16 00:43:43 -0300

    feat[Home]: page home created

--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,9 +1,52 @@
 <template>
-  <h1 class="font-black text-brand-main">Aqui é a home</h1>
+  <custom-header
+    @create-account="handleAccountCreate"
+    @login="handleLogin"
+  />
+
+  <contact />
+
+  <footer class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedbacker © 2022</p>
+  </footer>
 </template>
 
 <script>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import CustomHeader from './CustomHeader.vue'
+import Contact from './Contact.vue'
+
 export default {
-  name: 'home-page'
+  components: {
+    CustomHeader,
+    Contact
+  },
+  name: 'home-page',
+  setup () {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token-feedbacker')
+      if (token) {
+        router.push({
+          name: 'Feedbacks'
+        })
+      }
+    })
+
+    function handleLogin () {
+      return 'e'
+    }
+
+    function handleAccountCreate () {
+      return 'e'
+    }
+
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+  }
 }
 </script>
