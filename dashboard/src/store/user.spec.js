9454e024d4fc3d0aa4fd5bea10b53d0bc32ce39f
Rivanildo Júnior <rivanildo_junior7@hotmail.com>
2022-07-10 21:41:05 -0300

    feat[userStore]: aplicated test on functions userStore

---
+++ dashboard/src/store/user.spec.js
@@ -0,0 +1,39 @@
+import userStore from '@/hooks/useStore'
+import {
+  setCurrentUser,
+  resetUserStore,
+  setApiKey,
+  cleanCurrentUser
+} from './user'
+
+let store = null
+
+describe('UserStore', () => {
+  beforeEach(() => {
+    store = userStore()
+  })
+
+  afterEach(() => {
+    resetUserStore()
+  })
+
+  it('should set current user', () => {
+    setCurrentUser({ name: 'Rivanildo' })
+
+    expect(store.User.currentUser.name).toBe('Rivanildo')
+  })
+
+  it('should set API_KEY on current user', () => {
+    setApiKey('1234')
+
+    expect(store.User.currentUser.apiKey).toBe('1234')
+  })
+
+  it('should clean current user', () => {
+    setCurrentUser({ name: 'Rivanildo' })
+    expect(store.User.currentUser.name).toBe('Rivanildo')
+
+    cleanCurrentUser()
+    expect(store.User.currentUser.name).toBeFalsy()
+  })
+})
